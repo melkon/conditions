f8c0b1b7d438e9a77f299db75b3daecb534c8d56
melkon <andre@ziemek.de>
2010-10-27 20:26:26 +0200

    split up code files to get a better overview about what's going on
    ...and what is supposued to be used by outside of the condition systems.
    there will be more move-action in the future i suppose.

--- lib/conditions.rb
+++ lib/conditions.rb
@@ -1,152 +1,5 @@
-class Handler
-
-  @@conditions = {}
-
-  def self.get condition_name
-
-    return nil if (!@@conditions.has_key?(condition_name) or @@conditions[condition_name].empty?)
-
-    @@conditions[condition_name].reverse_each do |condition|
-
-      condition[:name] = condition_name
-      self.unset condition
-      
-      yield condition
-
-    end
-
-  end
-
-  def self.set condition
-
-    @@conditions[condition[:name]] = [] unless @@conditions[condition[:name]]
-    @@conditions[condition[:name]].push({:block => condition[:block],
-                                           :raise => condition[:raise]})
-
-  end
-
-  def self.unset condition
-
-    @@conditions[condition[:name]].each_with_index do |saved_condition, index|
-
-      if saved_condition[:block] == condition[:block]
-        
-        @@conditions[condition[:name]].delete_at index
-
-        # unset only one condition
-        return true
-
-      end
-
-    end
-
-  end
-
-  def self.unset_all condition_name
-
-    @@condition[condition_name] = Hash.new
-
-  end
-
-end
-
-class Restart
-
-  @@restarts = {}
-
-  def self.get restart_name
-
-    return nil if (!@@restarts.has_key?(restart_name) or @@restarts[restart_name].empty?)
-
-    @@restarts[restart_name].reverse_each do |restart|
-
-      restart[:name] = restart_name
-      self.unset restart
-      
-      yield restart
-
-    end
-
-  end
-
-  def self.set restart
-
-    @@restarts[restart[:name]] = [] unless @@restarts[restart[:name]]
-    @@restarts[restart[:name]].push({:block => restart[:block]})
-
-  end
-
-  def self.unset restart
-
-    @@restarts[restart[:name]].each_with_index do |saved_restart, index|
-
-      if saved_restart[:block] == restart[:block]
-        
-        @@restarts[restart[:name]].delete_at index
-
-        # unset only one restart
-        return true
-
-      end
-
-    end
-
-  end
-
-  def self.unset_all restart_name
-
-    @@restart[restart_name] = Hash.new
-
-  end
-
-end
-
-class ConditionNotHandledError < StandardError; end
-class ConditionHandledError < StandardError
-
-  attr_accessor :value, :condition
-
-  def initialize info
-
-    @value = info[:value]
-    @condition = info[:condition]
-
-  end
-
-end
-
-def parse_conditions conditions
-  
-  without_proc = []
-  parsed = []
-
-  conditions.each do |condition|
-
-    if condition.is_a? Hash
-
-      condition.each do |condition_name, condition_proc|
-
-        without_proc.each do |condition_name|
-          parsed.push :name => condition_name, :block => condition_proc
-        end
-
-        parsed.push :name => condition_name, :block => condition_proc
-
-      end
-
-      without_proc.clear
-
-    else
-
-      without_proc.push condition
-      
-    end
-
-  end
-
-  parsed
-
-end
+require "conditions_helpers"
+require "conditions_exceptions"
 
 def signal condition_name, *params
 
@@ -174,7 +27,7 @@ end
 
 def handle *conditions, &block
 
-  conditions = parse_conditions conditions
+  conditions = parse_handlers conditions
 
   conditions.each do |condition|
     Handler::set condition.merge! :raise => true
@@ -197,7 +50,7 @@ end
 
 def bind *conditions, &block
 
-  conditions = parse_conditions conditions
+  conditions = parse_handlers conditions
 
   conditions.each do |condition|
     Handler::set condition.merge! :raise => false
@@ -221,7 +74,7 @@ end
 
 def unbind *conditions
 
-  conditions = parse_conditions conditions
+  conditions = parse_handlers conditions
 
   conditions.each do |condition|
     Handler::unset condition
@@ -233,7 +86,7 @@ end
 
 def restart *restarts, &block
 
-  restarts = parse_conditions restarts
+  restarts = parse_handlers restarts
 
   restarts.each { |restart|  Restart::set restart }
 
